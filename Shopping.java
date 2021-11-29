1c87918c70f85ce37f9599d57852b3b41364a646
David <93246879+Arch887@users.noreply.github.com>
2021-11-29 23:38:34 +0200

    Update Shopping.java

--- Shopping.java
+++ Shopping.java
@@ -1,4 +1,11 @@
 public class Shopping{
+  public Shop{
+  }
+  
   public static void main(String[] args){
+    
+    int [] x = new int[]{1,2,3,7,6,4,8};
+    int[][][] y = new int[2][1][3];
+    
   }
  }
